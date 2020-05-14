81a81363e7141f6771929284b144bbdf4ab1d9ef
Raheim <raheimbbailey@gmail.com>
2020-05-14 00:47:40 -0400

    add base architecture for App and test scss and importing components

---
+++ sprint-1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import './partials/_reset.scss';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ sprint-1/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>
+                <div>
+                    <h1>Testing Font</h1>
+                    <p>Not working anymore?</p>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
