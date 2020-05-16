8009128fc14a38e6fedf4daa6d2b161330ea2672
Raheim <raheimbbailey@gmail.com>
2020-05-15 21:49:59 -0400

    add main vid html structure import state content

--- sprint-1/src/Components/MainVidContent/MainVidContent.js
+++ sprint-1/src/Components/MainVidContent/MainVidContent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './MainVidContent.scss'; 
+import view from './MainVidAssests/Icon-views.png';
+import likes from './MainVidAssests/Icon-likes.png';
 
 
 function MainVidContent(props) {
@@ -11,14 +13,19 @@ function MainVidContent(props) {
                 </div>
             </section>
             <div className="main-vid__info">
-                <h1 className="main-vid__header"></h1>
+                <h1 className="main-vid__header">{props.mainContent.title}</h1>
                 <aside className="main-vid__poster-info">
-                    <p className="main-vid__author"></p>
-                    <span className="main-vid__date"></span>
+                    <p className="main-vid__author">{props.mainContent.channel}</p>
+                    <span className="main-vid__date">{props.mainContent.timestamp}</span>
                 </aside>
                 <aside className="main-vid__viewer-info">
-                    
+                    <img src={view} alt="" className="main-vid__viewer-icon"/>
+                    <span className="main-vid__views">{props.mainContent.views}</span>
+                    <img src={likes} alt="" className="main-vid__likes-icon"/>
+                    <span className="main-vid__likes">{props.mainContent.likes}</span>
                 </aside>
+                <hr className="main-vid__split"/>
+                <p className="main-vid__desc">{props.mainContent.description}</p>
             </div>
         </div>
     )
