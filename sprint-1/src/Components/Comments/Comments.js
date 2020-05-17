0e3e283b46e8eb5712b07fb15f9033a2a79e6aa9
Raheim <raheimbbailey@gmail.com>
2020-05-16 23:59:04 -0400

    move Gen Comments back into Comments, add form & comments styles

--- sprint-1/src/Components/Comments/Comments.js
+++ sprint-1/src/Components/Comments/Comments.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Main from '../Main/Main';
 import {v4 as uuidv4} from 'uuid';
+import './Comments.scss';
 import MainVideoData from '../Main/MainData/MainVideoData';
 
 function Comments() {
     return (
         <section className="comments">
-            <h2></h2>
+            <h3 className="comments__head">3 Comments</h3>
             <div className="comments__ctn">
                 <div className="comments__form-ctn">
                     <form action="" method="POST" encType="" className="comments__form">
@@ -14,31 +15,45 @@ function Comments() {
                             <img className="comments__user-img" src="img placeholder"/>
                         </div>
                         <div className="comments__form-info-ctn">
-                            <h5 className="comments__title">Name</h5>
-                            <input className="comments__name" type="text" name="userName" label="Join the Conversation" placeholder="Enter Your Name" required=""/>
-                            <h5 className="comments__title-2">Comment</h5>
-                            <input className="comments__field" type="text" name="userVoice" placeholder="Add a new comment" required=""/>
+                            <h5 className="comments__title-2">Join the Conversation</h5>
+                            <input className="comments__field" type="text" name="userVoice" placeholder="" required=""/>
+                            <p className="comments__placeholder">That was easily the most spectacular BMX moment ever.</p>
                             <button className="comments__btn" type="submit">Comment</button>
                         </div>
                     </form>
                 </div>
-                    {/* <GenerateComments key={MainVideoData.id} /> */}
-
-                {/* <div className="comments__user-comment-ctn">
-                    <div className="comments__gen">
-                        <div className="comments__gen-ctn">
-                            <h4 className="comments__name"></h4>
-                            <p className="comments__time"></p>
-                            <p className="comments__comment"></p>
-                        </div>
-                        <div className="comments__img-ctn">
-                            <img src="img placeholder" className="comments__user-img"/>
-                        </div>
-                    </div>
-                </div> */}
+                    <GenerateComments key={MainVideoData.id} />
             </div>
         </section>
     )
+}
+
+class GenerateComments extends React.Component {
+    state = {
+        mainComments: MainVideoData.comments
+    }
+    render() {
+        return (
+            this.state.mainComments.map(comment =>{
+                return(
+                        <div key={comment.id} className="comments__user-comment-ctn">
+                            <div   className="comments__gen">
+                                <div  className="comments__gen-ctn">
+                                    <h4 className="comments__name">{comment.name}</h4>
+                                    <p className="comments__time">{comment.timestamp}</p>
+                                    <p className="comments__comment">{comment.comment}</p>
+                                </div>
+                                <div   className="comments__img-ctn">
+                                    <img   src="img placeholder" className="comments__user-img"/>
+                                </div>
+                            </div>
+                        </div>
+                    )
+            })
+        )
+       
+        
+    }
 }
 
 
--- sprint-1/src/Components/Main/Main.js
+++ sprint-1/src/Components/Main/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Main.scss'
 import MainVidContent from '../MainVidContent/MainVidContent';
 import Comments from '../Comments/Comments';
-import GenerateComments from '../GeneratedComments/GeneratedComments';
 import NextVideo from '../NextVideo/NextVideo';
 import MainVideoData from './MainData/MainVideoData';
 import {v4 as uuidv4} from 'uuid';
@@ -17,7 +16,6 @@ class Main extends React.Component {
             <main className="main-content">
                 <MainVidContent  mainContent={this.state.mainContent}/>
                 <Comments mainContent={this.state.mainContent}/>
-                <GenerateComments />
                 <NextVideo/>
             </main>
         )
