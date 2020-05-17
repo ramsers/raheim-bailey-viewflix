3b97759dc343cb073ba1af29abab2160429e4c7b
Raheim <raheimbbailey@gmail.com>
2020-05-17 10:14:14 -0400

    add Hero and main video tablet styles

--- sprint-1/src/Components/MainVidContent/MainVidContent.js
+++ sprint-1/src/Components/MainVidContent/MainVidContent.js
@@ -9,21 +9,23 @@ function MainVidContent(props) {
         <div className="main-vid">
             <section className="main-vid__hero">
                 <div className="main-vid__hero-ctn">
-                    <img src={props.mainContent.image} alt="" className="main-vid__hero-img"/>
+                    <img src={props.mainContent.image} alt="Two BMX Bikers on down slope" className="main-vid__hero-img"/>
                 </div>
             </section>
             <div className="main-vid__info">
                 <h1 className="main-vid__header">{props.mainContent.title}</h1>
-                <aside className="main-vid__poster-info">
-                    <span className="main-vid__author">{props.mainContent.channel}</span>
-                    <span className="main-vid__date">{props.mainContent.timestamp}</span>
-                </aside>
-                <aside className="main-vid__viewer-info">
-                    <img src={view} alt="" className="main-vid__viewer-icon"/>
-                    <span className="main-vid__views">{props.mainContent.views}</span>
-                    <img src={likes} alt="" className="main-vid__likes-icon"/>
-                    <span className="main-vid__likes">{props.mainContent.likes}</span>
-                </aside>
+                <div className="main-vid__info-ctn">
+                    <aside className="main-vid__poster-info">
+                        <span className="main-vid__author">{props.mainContent.channel}</span>
+                        <span className="main-vid__date">{props.mainContent.timestamp}</span>
+                    </aside>
+                    <aside className="main-vid__viewer-info">
+                        <img src={view} alt="" className="main-vid__viewer-icon"/>
+                        <span className="main-vid__views">{props.mainContent.views}</span>
+                        <img src={likes} alt="" className="main-vid__likes-icon"/>
+                        <span className="main-vid__likes">{props.mainContent.likes}</span>
+                    </aside>
+                </div> 
                 <hr className="main-vid__split"/>
                 <p className="main-vid__desc">{props.mainContent.description}</p>
             </div>
